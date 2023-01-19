ac7481e438291ddfe4b03844efc97809bc2c92bf
blekki <blekkiw@gmail.com>
2023-01-19 15:00:06 +0200

    Inital commit

---
+++ src/main/java/com/powersub/core/PowersubCoreApplication.java
@@ -0,0 +1,13 @@
+package com.powersub.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowersubCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowersubCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/powersub/core/PowersubCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powersub.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowersubCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
