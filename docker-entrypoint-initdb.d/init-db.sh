bbd785086e445bd0c5e3678e4f0afc12b9295c7e
Aleksandr D <abrin@ufs.travel>
2023-02-08 20:29:28 +0700

    In docker compose database changed name and credentials & restore env variables in application.yml

--- docker-entrypoint-initdb.d/init-db.sh
+++ docker-entrypoint-initdb.d/init-db.sh
@@ -2,8 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE ps_db;
+	CREATE DATABASE powersub;
 	CREATE USER powersub WITH LOGIN PASSWORD 'powersub@123';
-	GRANT ALL PRIVILEGES ON DATABASE ps_db TO powersub;
-	ALTER DATABASE ps_db OWNER TO powersub;
+	GRANT ALL PRIVILEGES ON DATABASE powersub TO powersub;
+	ALTER DATABASE powersub OWNER TO powersub;
 EOSQL
