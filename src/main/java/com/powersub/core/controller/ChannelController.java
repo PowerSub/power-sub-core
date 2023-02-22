eb98de2aab435c900110d9abcd3ccab42d139e77
GoPetr <gopetr@gmail.com>
2023-02-22 19:36:14 +0400

    create Channel CRUD.

--- src/main/java/com/powersub/core/controller/ChannelController.java
+++ src/main/java/com/powersub/core/controller/ChannelController.java
@@ -1,26 +1,68 @@
 package com.powersub.core.controller;
 
+import com.powersub.core.entity.Channel;
 import com.powersub.core.entity.ChannelDTO;
 import com.powersub.core.service.ChannelService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
+@RequestMapping("/channels")
 @RestController
 public class ChannelController {
 
   private final ChannelService channelService;
 
-  @PostMapping("/createchannel")
-   public ResponseEntity<ChannelDTO> createChannel(@RequestBody @Valid ChannelDTO channel)  {
+  @GetMapping()
+  public ResponseEntity<List<ChannelDTO>> getAll() {
+    //todo как это сделать с помощью стрима
+    List<Channel> channels = channelService.getChannels();
+    List<ChannelDTO> listDTO = new ArrayList<>();
+    for (Channel ch : channels) {
+      ChannelDTO chDTO = new ChannelDTO();
+      chDTO.setDescription(ch.getDescription());
+      chDTO.setTitle(ch.getTitle());
+      listDTO.add(chDTO);
+    }
+
+    return new ResponseEntity<>(listDTO, HttpStatus.OK);
+  }
+
+  @GetMapping("{id}")
+  public ResponseEntity<ChannelDTO> getChannel(@PathVariable Long id) {
+    Channel channelById = channelService.getChannelById(id);
+    ChannelDTO channelDTO = new ChannelDTO(channelById.getTitle(),
+            channelById.getDescription());
+
+    return new ResponseEntity<>(channelDTO, HttpStatus.OK);
+
+  }
+
+  @PostMapping("/create-channel")
+  public ResponseEntity<ChannelDTO> createChannel(@RequestBody @Valid ChannelDTO channel) {
     channelService.createChannel(channel);
     return new ResponseEntity<>(channel, HttpStatus.OK);
-   }
+  }
 
+  @PutMapping("{id}")
+  public ResponseEntity<ChannelDTO> updateChannel(@PathVariable Long id,
+                                                  @RequestBody @Valid ChannelDTO channelDTO) {
+
+    Channel channel = channelService.updateChannel(id, channelDTO);
+    channelDTO = new ChannelDTO(channel.getTitle(),
+            channel.getDescription());
+    return new ResponseEntity<>(channelDTO, HttpStatus.OK);
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<HttpStatus> deleteChannel(@PathVariable Long id) {
+    channelService.delete(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
 }
--- src/main/java/com/powersub/core/service/ChannelService.java
+++ src/main/java/com/powersub/core/service/ChannelService.java
@@ -2,15 +2,30 @@ package com.powersub.core.service;
 
 import com.powersub.core.entity.Channel;
 import com.powersub.core.entity.ChannelDTO;
+import com.powersub.core.repository.AccountRepository;
 import com.powersub.core.repository.ChannelRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ChannelService {
 
   private final ChannelRepository channelRepository;
+  private final AccountRepository accountRepository;
+
+  public List<Channel> getChannels() {
+    return channelRepository.findAll();
+  }
+
+  public Channel getChannelById(Long id) {
+
+    Optional<Channel> foundChannel = channelRepository.findById(id);
+    return foundChannel.orElse(null);
+  }
 
   public void createChannel(ChannelDTO channel) {
     Channel ch = Channel.builder().
@@ -19,5 +34,19 @@ public class ChannelService {
             build();
 
     channelRepository.save(ch);
+  }
+
+  public Channel updateChannel(Long id, ChannelDTO channelDTO) {
+    Optional<Channel> channelToUpdate = channelRepository.findById(id);
+    if (channelToUpdate.isPresent()) {
+      channelToUpdate.get().setTitle(channelDTO.getTitle());
+      channelToUpdate.get().setDescription(channelDTO.getDescription());
+      channelRepository.save(channelToUpdate.orElseThrow());
+    }
+    return channelToUpdate.orElse(null);
+  }
+
+  public void delete(Long id) {
+    channelRepository.deleteById(id);
   }
 }
